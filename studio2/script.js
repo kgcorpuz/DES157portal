cda775d5b7abdfb027a74e7813e1d570331d28da
Kathryn Corpuz <kvcorpuz@ucdavis.edu>
2023-03-02 22:31:38 -0800

    added messages

--- studio2/script.js
+++ studio2/script.js
@@ -28,35 +28,35 @@
            case 'suman': theImg.className = 'suman'; 
             document.getElementById('overlay').className="showing";
             document.querySelector('h2').innerHTML = "Suman";
-            document.querySelector('div p').innerHTML = "Suman is a sticky rice dessert. It is prepared by steaming the glutinous rice and coconut milk mixture in a banana leaf. It has a nice mild flavor. It's often eaten as a side dish!";
+            document.querySelector('div p').innerHTML = "Suman is a sticky rice dessert. It is prepared by steaming the glutinous rice and coconut milk mixture in a banana leaf. It has a nice mild flavor. It's often eaten as a side dish! I remember making this with my grandma when I was little.";
             document.getElementById('dessert_img').src=`images/${myImages[0]}`;
             break;
 
            case 'buko': theImg.className = 'buko';
            document.getElementById('overlay').className="showing";
            document.querySelector('h2').innerHTML = "Buko Pandan";
-           document.querySelector('div p').innerHTML = "Buko Pandan is a coconut based dessert. There are many different mix-ins of jello, coconut shreds and nata de coco. It's often colored green with padan leaf flavoring.";
+           document.querySelector('div p').innerHTML = "Buko Pandan is a coconut based dessert. There are many different mix-ins of jello, coconut shreds and nata de coco. It's often colored green with padan leaf flavoring. I love this dessert so much I always make it for christmas celebrations.";
            document.getElementById('dessert_img').src=`images/${myImages[1]}`;
            break;
 
            case 'puto': theImg.className = 'puto';
            document.getElementById('overlay').className="showing";
            document.querySelector('h2').innerHTML = "Puto";
-           document.querySelector('div p').innerHTML = "Puto is a steamed rice cake dessert. It is made out of rice flour, coconut milk and sugar. It's steamed for the texture to be nice and chewy.";
+           document.querySelector('div p').innerHTML = "Puto is a steamed rice cake dessert. It is made out of rice flour, coconut milk and sugar. It's steamed for the texture to be nice and chewy. My lola would make the best ones for special occasions.";
            document.getElementById('dessert_img').src=`images/${myImages[2]}`;
            break;
 
            case 'taho': theImg.className = 'taho';
            document.getElementById('overlay').className="showing";
            document.querySelector('h2').innerHTML = "Taho";
-           document.querySelector('div p').innerHTML = "Taho is a silken tofu dessert. It is often served hot with a thick sugar syrup and boba balls. I enjoy eating it on a cold winter day to warm up!";
+           document.querySelector('div p').innerHTML = "Taho is a silken tofu dessert. It is often served hot with a thick sugar syrup and boba balls. I enjoy eating it on a cold winter day to warm up! It always makes me feel nostalgic of my hometown.";
            document.getElementById('dessert_img').src=`images/${myImages[3]}`;
            break;
 
            case 'turon': theImg.className = 'turon';
            document.getElementById('overlay').className="showing";
            document.querySelector('h2').innerHTML = "Turon";
-           document.querySelector('div p').innerHTML = "Turon is a crunchy dessert that is a banana with jack fruit wrapped in a lumpia wrapper and fried. It is coated in a sweet sugar syrup for the glossy look.";
+           document.querySelector('div p').innerHTML = "Turon is a crunchy dessert that is a banana with jack fruit wrapped in a lumpia wrapper and fried. It is coated in a sweet sugar syrup for the glossy look. I always bring this for my friends to try out because of the sweet and flaky texture!";
            document.getElementById('dessert_img').src=`images/${myImages[4]}`;
            break;
 
